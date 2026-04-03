3d8de2a6b4d94419b0819d0baab1282dd04354f1
Chris Frantz <cfrantz@google.com>
2026-04-03 08:22:03 -0700

    Initialize the Open Market SKU Configuration
    The current configuration has an OTP configuraion, empty personalization
    and ft extensions, orchestrator configurations and an initial set of
    fake keys.  Fake keys will be replaced when we generate real keys.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ skus/open/keys/owner/README.md
@@ -0,0 +1,6 @@
+# Owner and application keys
+
+The keys in this directory are the owner and application keys for the manufacturing owner.
+The private components of these keys are stored in TBD.
+
+These keys were generated using TBD.
---
+++ skus/open/personalize_ext.c
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/manuf/base/personalize_ext.h"
+
+status_t personalize_extension_pre_cert_endorse(
+    personalize_extension_pre_endorse_t *pre_params) {
+  return OK_STATUS();
+}
+
+status_t personalize_extension_post_cert_endorse(
+    personalize_extension_post_endorse_t *post_params) {
+  return OK_STATUS();
+}
